16db776d461de3426c77e746847442efa9e4ba80
Youssef Barakat <youssefbarakat440@gmail.com>
2024-12-29 13:57:13 +0200

    1
    w

--- about-us-form.js
+++ about-us-form.js
@@ -9,23 +9,23 @@ function f1()
    if (x=="" || x==null)
    {
     //alert ('you must enter your first name, it cannot be empty')
-    document.getElementById("myerror").innerHTML='Please fill the all of the form, do not leave any filed empty';
+    document.getElementById("myerror").innerHTML='Please enter your first name, do not leave any filed empty';
    }
     else if (y=="" || y==null)
     {
-     document.getElementById("myerror").innerHTML='Please fill the all of the form, do not leave any filed empty';
+     document.getElementById("myerror").innerHTML='Please enter your last name, do not leave any filed empty';
     }
     else if (z=="" || z==null)
         {
-         document.getElementById("myerror").innerHTML='Please fill the all of the form, do not leave any filed empty';
+         document.getElementById("myerror").innerHTML='Please enter your email, do not leave any filed empty';
         }
         else if (t=="" || t==null)
             {
-             document.getElementById("myerror").innerHTML='Please fill the all of the form, do not leave any filed empty';
+             document.getElementById("myerror").innerHTML='Please enter your phone number, do not leave any filed empty';
             }
-            else if (r=="" || r==null)
+            else if (r=="" || r==null || r=="What information would you like to see added to our About Us page?")
                 {
-                 document.getElementById("myerror").innerHTML='Please fill the all of the form, do not leave any filed empty';
+                 document.getElementById("myerror").innerHTML='Please enter your suggestion, do not leave any filed empty';
                 }
     else{
         alert('thanks for filling the form')
