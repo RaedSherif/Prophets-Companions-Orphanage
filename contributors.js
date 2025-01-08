afd61518dc6071c97242261519397ad58e39913b
Raed <raedaasherif@gmail.com>
2025-01-08 10:30:29 +0200

    updated js to contributors

---
+++ contributors.js
@@ -0,0 +1,25 @@
+function partner()
+{
+    let name = document.getElementById("name").value.trim();
+    let organization = document.getElementById("organization").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let details = document.getElementById("partnershipDescription").value.trim();
+    let error = document.getElementById('error');
+
+
+    if (error) {
+        error.textContent = '';
+      }
+
+    
+    if (!name) {
+        error.textContent = 'Please enter your full name. Do not leave this field empty.';
+        return false;
+    }
+
+    if (name.length > 7)
+    {
+        error.textContent ='Name can not be longer than 32 characters';
+        return false;
+    }
+}
